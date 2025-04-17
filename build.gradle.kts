8bf571dab357a9085bd8474f016694f630863f50
FegrusB <fergus@beckerleg.co.uk>
2025-04-17 11:15:58 +0100

    Removed unused imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+    kotlin("jvm") version "2.1.20"
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
