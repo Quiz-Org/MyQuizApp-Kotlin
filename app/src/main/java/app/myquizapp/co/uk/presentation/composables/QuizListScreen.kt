7a7891e71cdde2c5e3f8867a794afb9eb1775c1e
FegrusB <fergus@beckerleg.co.uk>
2025-04-22 16:22:20 +0100

    prittier QuizListScreen.kt

--- app/src/main/java/app/myquizapp/co/uk/presentation/composables/QuizListScreen.kt
+++ app/src/main/java/app/myquizapp/co/uk/presentation/composables/QuizListScreen.kt
@@ -1,46 +1,43 @@
 package app.myquizapp.co.uk.presentation.composables
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import app.myquizapp.co.uk.domain.quiz.Quiz
 import app.myquizapp.co.uk.presentation.QuizState
-
-@Composable
-fun QuizRow(
-    modifier: Modifier,
-    quiz: Quiz
-) {
-    Card(modifier = Modifier.padding(8.dp)) {
-        Column(
-            modifier = Modifier.padding(8.dp),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(quiz.name)
-            Text(quiz.desc)
-        }
-    }
-}
-
 
 @Composable
 fun QuizListScreen(
     state: QuizState,
-    modifier: Modifier = Modifier
 ) {
-    state.quizzes?.let { data ->
-        LazyColumn {
-            items(data) { quiz ->
-                QuizRow(modifier.fillParentMaxWidth(), quiz)
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = innerPadding
+        )
+        {
+            items(items = state.quizzes.orEmpty()) { quiz ->
+                ListItem(
+                    headlineContent = { Text(quiz.name) },
+                    supportingContent = { Text(quiz.desc) },
+                    overlineContent = { Text(quiz.category.toString()) },
+                    leadingContent = {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            tint = quiz.colour
+                        )
+                    }
+                )
             }
-         }
-    }
 
+        }
+    }
 }
